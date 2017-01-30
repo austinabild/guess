ddbb2acf269480b1faeb07d360d0697572807728
Austin Abild <abildae25@mounties.mansfield.edu>
2017-01-30 09:09:03 -0500

    Modified logic to indicate a high or low guess

--- src/guess/Guess.java
+++ src/guess/Guess.java
@@ -2,7 +2,7 @@
  * Author:      Austin Abild
  * Program:     Guessing Game
  * Purpose:     Allow the user to guess a number 1-10 until they guess the
- *              randomly generated number. More to come...
+ *              randomly generated number.
  * Date:        1/27/2017
  */
 package guess;
@@ -12,7 +12,6 @@ import java.util.Scanner;
 
 public class Guess {
     
-
     public static void main(String[] args) {
         Random rand = new Random();
         Scanner ui = new Scanner(System.in);
@@ -24,13 +23,18 @@ public class Guess {
         while(true)
         {
             System.out.println("Please guess a number 1-10.");
-        if(ui.nextInt() == compNumber)
+            int userGuess = ui.nextInt();
+        if(userGuess == compNumber)
         {
             break;
         }
-        else
+        else if(userGuess > compNumber)
         {
-            System.out.println("Sorry, please try again!");
+            System.out.println("Sorry, you guessed too HIGH!");
+        }
+        else if(userGuess < compNumber)
+        {
+            System.out.println("Sorry, you guessed too LOW!");
         }
         }// end while
         System.out.println("Congratulations, you guessed correctly!");
